6009dc6f030a2d7d73a6128c4a5d6a494f656e67
rizkyrsyd28 <rizkyrasyid28@gmail.com>
2023-02-27 19:49:22 +0700

    main

--- src/main.py
+++ src/main.py
@@ -1,68 +1,67 @@
 from IO import IO
 
-# if __name__ == "__main__":
-#     exit : bool = False
-
-# <<<<<<< HEAD
-#     IO.landing(IO)
-#     User = IO()
-#     while not exit :
-#         if User.mode == 0 :
-#             exit = True
-#         if User.mode == 1:
-#             User.set_number()
-#             User.set_dimensi()
-#             User.printResult()
-#             User.ask_next()
-#         if User.mode == 2:
-#             User.visual()
+if __name__ == "__main__":
+    exit : bool = False
+    
+    IO.landing(IO)
+    User = IO()
+    while not exit :
+        if User.mode == 0 :
+            exit = True
+        if User.mode == 1:
+            User.set_number()
+            User.set_dimensi()
+            User.printResult()
+            User.ask_next()
+        if User.mode == 2:
+            User.visual()
             
         
-=======
-from dataType import point, couple
-from bruteForce import bruteForce
-from divideConquer import divideConquer
 
-#debug
-import time as t
-import random as rand
-import numpy as np
+# from dataType import point, couple
+# from bruteForce import bruteForce
+# from divideConquer import divideConquer
 
-def generateRandom(n:int, dim:int):
-    points = np.empty((n), dtype=point)
+# #debug
+# import time as t
+# import random as rand
+# import numpy as np
 
-    for i in range(n):
-        val = np.empty((dim), dtype=float)
-        for j in range(dim):
-            val[j] = rand.uniform(-10**6, 10**6)
+# def generateRandom(n:int, dim:int):
+#     points = np.empty((n), dtype=point)
 
-        points[i] = point(dim,val)
+#     for i in range(n):
+#         val = np.empty((dim), dtype=float)
+#         for j in range(dim):
+#             val[j] = rand.uniform(-10**6, 10**6)
+
+#         points[i] = point(dim,val)
     
-    return points
+#     return points
 
 
-def main():
-    n:int = int(input("Masukkan jumlah titik : "))
-    dim:int = int(input("Masukkan dimensi titik : "))
-    # n = 2**10
-    points = generateRandom(n, dim)
+# def main():
+#     n:int = int(input("Masukkan jumlah titik : "))
+#     dim:int = int(input("Masukkan dimensi titik : "))
+#     # n = 2**10
+#     points = generateRandom(n, dim)
     
-    startBF = t.time() 
-    closestCoupleBF, numBF = bruteForce(points)
-    stopBF = t.time()
-    print("Brute Force : ", closestCoupleBF)
-    print("number of euclidean op : ", numBF)
-    print("Waktu BF : ", stopBF-startBF, " detik\n")
+#     startBF = t.time() 
+#     closestCoupleBF, numBF = bruteForce(points)
+#     stopBF = t.time()
+#     print("Brute Force : ", closestCoupleBF)
+#     print("number of euclidean op : ", numBF)
+#     print("Waktu BF : ", stopBF-startBF, " detik\n")
 
-    startDnC = t.time()
-    closestCoupleDnC, numDnC = divideConquer(sorted(points))
-    stopDnC = t.time()
-    print("DnC : ", closestCoupleDnC)
-    print("number of Euclidean : ", numDnC)
-    print("Waktu DnC : ", stopDnC-startDnC, " detik")
-    # pair = divideConquer(List)
+#     startDnC = t.time()
+#     closestCoupleDnC, numDnC = divideConquer(sorted(points))
+#     stopDnC = t.time()
+#     print("DnC : ", closestCoupleDnC)
+#     print("number of Euclidean : ", numDnC)
+#     print("Waktu DnC : ", stopDnC-startDnC, " detik")
+#     # pair = divideConquer(List)
 
-    # print(pair)
+#     # print(pair)
 
-main()
->>>>>>> c8e1e7d34aab5f9f80f4e520922799cd915a8c60
+# main()
+
